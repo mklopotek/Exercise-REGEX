f7b25d6a11ec2a2188514b1e3fcbf52612e17ef1
Magda Klopotek <magda.klopotek@gmail.com>
2018-09-08 14:34:35 +0200

    regex expression added

--- src/ValidationForms.js
+++ src/ValidationForms.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-const ValidationForms = (props) => (
-    <div>
-        <h1>New User Sign Up</h1>
-        <form>
+class ValidationForms extends React.Component {
 
-            <input type="text" name="username" placeholder="username" />
-            <p>Username must be  and contain 5 - 12 characters</p>
+    validationTest = (event) => {
+        let value = new String(event.target.value);
+        let fieldType = event.target.name;
 
-            <input type="text" name="email" placeholder="email" />
-            <p>Email must be a valid address, e.g. me@mydomain.com</p>
+        switch(fieldType){
+            case 'username':
 
-            <input type="password" name="password" placeholder="password" />
-            <p>Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters</p>
+            case 'email':
+            
+            case 'password':
 
-            <input type="text" name="telephone" placeholder="telephone" />
-            <p>Telephone must be a valid PL telephone number (11 digits)</p>
+            case 'telephone':
 
-            <input type="text" name="slug" placeholder="user login" />
-            <p>Login must contain only lowercase letters, numbers and hyphens and be 8 - 20 characters</p>
+        }
 
-        </form>
-    </div>
-)
+    };
 
-export default ValidationForms
+    render() {
+        const usernameReg = new RegExp('^[/w]{5,12}$', 'gi');
+        const emailReg = /^(\w)+@(\w)+.[a-z]{2,3}$/gi;
+        const passwordReg = /^[a-z0-9]{8,20}$/gi;
+        const telephoneReg = /^\+48 [1-9][0-9]{2} [0-9]{3} [0-9]{3}$/g;
+
+        return (
+        <div>
+            <h1>Sign Up</h1>
+            <form>
+                <input type="text" name="username" placeholder="username" onChange={this.validationTest} />
+                <p>Username must be  and contain 5 - 12 characters</p>
+
+                <input type="text" name="email" placeholder="email" />
+                <p>Email must be a valid address, e.g. me@mydomain.com</p>
+
+                <input type="text" name="password" placeholder="password" />
+                <p>Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters</p>
+
+                <input type="text" name="telephone" placeholder="telephone" />
+                <p>Telephone must be a valid PL telephone number (11 digits) like this: +48 111 111 111</p>
+
+            </form>
+        </div>
+    )};
+};
+
+export default ValidationForms;
