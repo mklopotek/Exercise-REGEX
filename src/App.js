76fa553c7f0ec405043d74ee6f8c2c9c374de62a
Magda Klopotek <magda.klopotek@gmail.com>
2018-09-09 23:20:06 +0200

    MuiThemeProvider from material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import ValidationForms from './ValidationForms';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <ValidationForms />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
